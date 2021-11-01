65cdd147f8c381ff654146ead68e5a0a10f97f1b
yujeong lim <anneu5712@gmail.com>
2021-11-01 22:19:08 +0900

    bs4 package(크롤링) prac

--- week3_prac.py
+++ week3_prac.py
@@ -1,12 +1,28 @@
-import requests # requests 라이브러리 설치 필요
+import requests
+from bs4 import BeautifulSoup
 
-r = requests.get('http://openapi.seoul.go.kr:8088/6d4d776b466c656533356a4b4b5872/json/RealtimeCityAir/1/99')
-rjson = r.json()
+# 타겟 URL을 읽어서 HTML를 받아오고,
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get('https://movie.naver.com/movie/sdb/rank/rmovie.nhn?sel=pnt&date=20200303',headers=headers)
 
-gus = rjson['RealtimeCityAir']['row']
+# HTML을 BeautifulSoup이라는 라이브러리를 활용해 검색하기 용이한 상태로 만듦
+# soup이라는 변수에 "파싱 용이해진 html"이 담긴 상태가 됨
+# 이제 코딩을 통해 필요한 부분을 추출하면 된다.
+soup = BeautifulSoup(data.text, 'html.parser')
 
-for gu in gus :
-    gu_name = gu['MSRSTE_NM']
-    gu_mise = gu['IDEX_MVL']
-    if (gu_mise > 65) :
-        print(gu_name, gu_mise)
+
+# select를 이용해서, tr들을 불러오기
+# old_content > table > tbody > tr:nth-child(2)
+# old_content > table > tbody > tr:nth-child(3)
+# 한개당 태그 ➡ # old_content > table > tbody > tr
+movies = soup.select('#old_content > table > tbody > tr')
+
+# movies (tr들) 의 반복문을 돌리기
+for movie in movies:
+    # movie 안에 a 가 있으면,
+    a_tag = movie.select_one('td.title > div > a')
+    if a_tag is not None:
+        rank = movie.select_one('td:nth-child(1) > img')['alt']   # img 태그의 alt 속성값을 가져오기
+        title = a_tag.text                                        # a 태그 사이의 텍스트를 가져오기
+        star = movie.select_one('td.point').text                  # td 태그 사이의 텍스트를 가져오기
+        print(rank, title, star)
