1bc52b74bbd538125ef75489ef3a451088e4b7ad
yujeong lim <anneu5712@gmail.com>
2021-11-01 17:49:20 +0900

    requests package prac

--- week3_prac.py
+++ week3_prac.py
@@ -1,9 +1,12 @@
-people = [{'name': 'bob', 'age': 20},
-          {'name': 'carry', 'age': 38},
-          {'name': 'john', 'age': 7},
-          {'name': 'smith', 'age': 17},
-          {'name': 'ben', 'age': 27}]
+import requests # requests 라이브러리 설치 필요
 
-for person in people :
-    if person['age'] < 20 :
-        print(person)
+r = requests.get('http://openapi.seoul.go.kr:8088/6d4d776b466c656533356a4b4b5872/json/RealtimeCityAir/1/99')
+rjson = r.json()
+
+gus = rjson['RealtimeCityAir']['row']
+
+for gu in gus :
+    gu_name = gu['MSRSTE_NM']
+    gu_mise = gu['IDEX_MVL']
+    if (gu_mise > 65) :
+        print(gu_name, gu_mise)
