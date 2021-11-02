9cff99a875a2c1a721c31fba3bd4386c121079ca
yujeong lim <anneu5712@gmail.com>
2021-11-02 15:37:33 +0900

    pymongo_insert

---
+++ week3_dbprac.py
@@ -0,0 +1,11 @@
+from pymongo import MongoClient           # pymongo를 임포트 하기(패키지 인스톨 먼저 해야겠죠?)
+client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
+db = client.dbpymongo                     # 'dbpymongo'라는 이름의 db를 만듭니다.
+
+# insert
+
+# 'users'라는 collection에 넣습니다.
+db.users.insert_one({'name':'bobby','age':21})
+db.users.insert_one({'name':'kay','age':27})
+db.users.insert_one({'name':'john','age':30})
+db.users.insert_one({'name':'jane','age':21})
