fe92076d320a3973ebc5b6d3cc6e0a370d87e349
yujeong lim <anneu5712@gmail.com>
2021-11-02 15:48:33 +0900

    pymongo-find

--- week3_dbprac.py
+++ week3_dbprac.py
@@ -2,10 +2,10 @@ from pymongo import MongoClient           # pymongo를 임포트 하기(패키�
 client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
 db = client.dbpymongo                     # 'dbpymongo'라는 이름의 db를 만듭니다.
 
-# insert
+# find - 모든 결과 값을 보기
 
-# 'users'라는 collection에 넣습니다.
-db.users.insert_one({'name':'bobby','age':21})
-db.users.insert_one({'name':'kay','age':27})
-db.users.insert_one({'name':'john','age':30})
-db.users.insert_one({'name':'jane','age':21})
+# mogoDB에 있는 데이터 모두 보기
+all_users = list(db.users.find({},{'_id':False}))
+# 반복문을 돌며 모든 결과값을 보기
+for users in all_users:
+    print(users)
