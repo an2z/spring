255faf2f081d2d19c3b6d8468f34d7f5da9c4f69
yujeong lim <anneu5712@gmail.com>
2021-11-18 01:59:20 +0900

    파이썬 반복문

--- basic3.py
+++ basic3.py
@@ -10,4 +10,21 @@ elif a == b :                    # 조건을 추가 (elif)
     print('a는 b와 같다')
 else :
     print('a는 b보다 크다')
-    
+
+
+# for 반복문
+
+for i in range(10) :             # i는 0부터 10까지 1씩 증가를 반복
+    print(i)
+
+aaa = [1,2,3,4,5,6,7,8]
+for i in aaa :                   # 리스트 안에 있는 값을 하나씩 꺼내옴
+    print(i)
+
+
+# while 반복문
+
+i = 0                            # i는 0부터 10까지 1씩 증가를 반복
+while(i<10):
+    print(i)
+    i += 1   #i=i+1
