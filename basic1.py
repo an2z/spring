eccbab4696684ad4468bba341113d2175f29b370
yujeong lim <anneu5712@gmail.com>
2021-11-17 17:01:16 +0900

    파이썬 변수타입

---
+++ basic1.py
@@ -0,0 +1,26 @@
+# 파이썬 변수타입 ( 정수 | 실수 | boolean | null )
+
+# 정수,실수
+a = 1
+b = 2.0
+c = a/b
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+
+# boolean - 참과 거짓을 가지는 변수 (false/true) 
+d = False
+print(d)
+print(type(d))
+
+
+# null - 정수도 실수도 아닌 어떠한 값도 아니라는 의미
+f = None
+print(f)
+print(type(f))
