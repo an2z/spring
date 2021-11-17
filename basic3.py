109083a53687459e2f09e5754e1721684a346c35
yujeong lim <anneu5712@gmail.com>
2021-11-18 01:28:39 +0900

    파이썬 조건문-elif,and,or

--- basic3.py
+++ basic3.py
@@ -1,10 +1,13 @@
 # 조건문
 
-a = 1
-b = 3
+a = 3
+b = 5
+c = 1
 
-if a < b : 
+if a < b and a < c :             # 조건문은 중복이 가능 (and/or)
     print('a는 b보다 작다')
+elif a == b :                    # 조건을 추가 (elif)
+    print('a는 b와 같다')
 else :
     print('a는 b보다 크다')
     
