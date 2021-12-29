03e1e0f994c8b5a1218e0ba31c2728a0597571cf
yujeong lim <anneu5712@gmail.com>
2021-12-29 23:34:45 +0900

    변수, 자료형

---
+++ ch02/BooleanExam.java
@@ -0,0 +1,17 @@
+package ch02;
+
+/* 논리 자료형 */
+public class BooleanExam {
+	public static void main(String[] args) {		
+		boolean b1 = true;
+		boolean b2 = false;		
+		System.out.println(b1);  // b1이 지닌 값 출력
+		System.out.println(b2);
+		
+		int n1 = 10;
+		int n2 = 20;
+		System.out.println(n1 < n2);
+		System.out.println(n1 > n2);
+	}
+
+}
