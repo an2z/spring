219d8105321b5f2fbeb43df818c3b95fc77a9257
yujeong lim <anneu5712@gmail.com>
2022-01-25 22:24:41 +0900

    컬렉션 기반 알고리즘 (sort, binarySearch, copy)

---
+++ ch24/SortCollections_Car.java
@@ -0,0 +1,39 @@
+package ch24;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+/* 컬렉션 기반 알고리즘 (정렬) */
+
+class Car implements Comparable<Car> {
+	private int disp; // 배기량
+
+	public Car(int disp) {
+		this.disp = disp;
+	}
+
+	@Override
+	public String toString() {
+		return "cc: " + disp;
+	}
+
+	@Override
+	public int compareTo(Car o) {
+		return disp - o.disp; // 오름차순
+	}
+}
+
+class SortCollections_Car {
+	public static void main(String[] args) {
+		List<Car> list = new ArrayList<>();
+		list.add(new Car(1200));
+		list.add(new Car(3000));
+		list.add(new Car(1800));
+		Collections.sort(list); // 정렬
+		
+		for (Iterator<Car> itr = list.iterator(); itr.hasNext();)
+			System.out.println(itr.next() + " ");
+	}
+}
