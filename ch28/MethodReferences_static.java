f2483b2fb6da34af5a39b86bd55b264add5087c9
yujeong lim <anneu5712@gmail.com>
2022-02-04 21:48:31 +0900

    메소드 참조

---
+++ ch28/MethodReferences_static.java
@@ -0,0 +1,22 @@
+package ch28;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Consumer;
+
+/* static 메소드 참조 */
+
+class MethodReferences_static {
+	public static void main(String[] args) {
+		List<Integer> li = Arrays.asList(1, 3, 5, 7, 9);
+		li = new ArrayList<>(li);
+		
+		// Consumer<List<Integer>> c = l -> Collections.reverse(l); // 람다식
+		Consumer<List<Integer>> c = Collections::reverse; // 메소드 참조
+		c.accept(li); // 순서 뒤집기 진행
+		System.out.println(li); // 출력
+	}
+}
+// Consumer<T>  void accept(T t)
