b54109fb646e092e900e3baeea440c1d2b739d60
yujeong lim <anneu5712@gmail.com>
2022-05-04 18:17:47 +0900

    로그인/로그아웃 처리 (쿠키 사용)

--- src/main/java/hello/login/web/HomeController.java
+++ src/main/java/hello/login/web/HomeController.java
@@ -1,16 +1,41 @@
 package hello.login.web;
 
+import hello.login.domain.member.Member;
+import hello.login.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Slf4j
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
 
-    @GetMapping("/")
+    private final MemberRepository memberRepository;
+
+//    @GetMapping("/")
     public String home() {
         return "home";
+    }
+
+    @GetMapping("/")
+    public String homeLogin(@CookieValue(name = "memberId", required = false) Long memberId, Model model) {
+        // 로그인 쿠키가 없는 경우
+        if (memberId == null) {
+            return "home";
+        }
+
+        // 로그인 쿠키가 있는 경우
+        Member loginMember = memberRepository.findById(memberId);
+        if (loginMember == null) { // 쿠키가 있어도 회원이 없는 경우
+            return "home";
+        }
+
+        model.addAttribute("member", loginMember);
+        return "loginHome"; // 로그인 사용자 전용 홈 화면으로 이동
     }
 
 }
--- src/main/java/hello/login/web/login/LoginController.java
+++ src/main/java/hello/login/web/login/LoginController.java
@@ -1,5 +1,6 @@
-package hello.login.domain.login;
+package hello.login.web.login;
 
+import hello.login.domain.login.LoginService;
 import hello.login.domain.member.Member;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -9,6 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 @Slf4j
@@ -24,7 +27,7 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public String login(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult) {
+    public String login(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult, HttpServletResponse response) {
         // 검증 실패
         if (bindingResult.hasErrors()) {
             return "login/loginForm";
@@ -40,9 +43,24 @@ public class LoginController {
             return "login/loginForm";
         }
 
-        // 로그인 성공 처리 TODO
+        // 로그인 성공 처리
+        // 쿠키 생성 (시간 정보를 주지 않으면 세션 쿠키 -> 브라우저 종료시 모두 종료)
+        Cookie idCookie = new Cookie("memberId", String.valueOf(loginMember.getId()));
+        response.addCookie(idCookie);
 
         return "redirect:/"; // 홈 화면으로 이동
+    }
+
+    @PostMapping("/logout")
+    public String logout(HttpServletResponse response) {
+        expireCookie(response, "memberId");
+        return "redirect:/";
+    }
+
+    private void expireCookie(HttpServletResponse response, String cookieName) {
+        Cookie cookie = new Cookie(cookieName, null); // 응답 쿠키 생성
+        cookie.setMaxAge(0); // 0으로 설정해 즉시 종료
+        response.addCookie(cookie);
     }
 
 }
