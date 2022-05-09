008c480de2b17d03e08a8efcca2d1ec9b9d13ef1
yujeong lim <anneu5712@gmail.com>
2022-05-10 00:23:29 +0900

    로그인/로그아웃 처리 (스프링 @SessionAttribute 사용)

--- src/main/java/hello/login/web/HomeController.java
+++ src/main/java/hello/login/web/HomeController.java
@@ -5,10 +5,12 @@ import hello.login.domain.member.MemberRepository;
 import hello.login.web.session.SessionManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.web.servlet.server.Session;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -56,7 +58,7 @@ public class HomeController {
         return "loginHome"; // 로그인 사용자 전용 홈 화면으로 이동
     }
 
-    @GetMapping("/")
+//    @GetMapping("/")
     public String homeLoginV3(HttpServletRequest request, Model model) {
         // 조회 시 세션이 없으면 홈 화면으로 이동
         HttpSession session = request.getSession(false);
@@ -66,6 +68,20 @@ public class HomeController {
 
         Member loginMember = (Member) session.getAttribute(SessionConst.LOGIN_MEMBER);
         // 세션에 회원 정보가 없으면 홈 화면 으로 이동
+        if (loginMember == null) {
+            return "home";
+        }
+
+        // 세션이 유지되면 로그인 사용자 전용 홈 화면으로 이동
+        model.addAttribute("member", loginMember);
+        return "loginHome";
+    }
+
+    @GetMapping("/")
+    public String homeLoginV3Spring(
+            @SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember
+            , Model model) {
+        // 조회 시 세션이 없으면 홈 화면으로 이동
         if (loginMember == null) {
             return "home";
         }
