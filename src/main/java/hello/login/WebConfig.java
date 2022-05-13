dbd193a82749de2d71baf5676bb09f1bcf5aaa37
yujeong lim <anneu5712@gmail.com>
2022-05-13 21:30:16 +0900

    요청 로그 (서블릿 필터)

---
+++ src/main/java/hello/login/WebConfig.java
@@ -0,0 +1,26 @@
+package hello.login;
+
+import hello.login.web.filter.LogFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.Filter;
+
+@Configuration
+public class WebConfig {
+
+    /**
+     * 필터 등록
+     */
+    @Bean
+    public FilterRegistrationBean logFilter() {
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new LogFilter()); // 등록할 필터를 지정하기
+        filterRegistrationBean.setOrder(1); // 필터는 체인으로 동작하므로, 순서를 지정
+        filterRegistrationBean.addUrlPatterns("/*"); // 필터를 적용할 url 패턴 지정
+
+        return filterRegistrationBean;
+    }
+
+}
