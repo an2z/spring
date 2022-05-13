2a299553ddbe1919e8b9145164e4363d336a3e42
yujeong lim <anneu5712@gmail.com>
2022-05-13 23:15:43 +0900

    인증 체크 (서블릿 필터)

--- src/main/java/hello/login/WebConfig.java
+++ src/main/java/hello/login/WebConfig.java
@@ -1,6 +1,7 @@
 package hello.login;
 
 import hello.login.web.filter.LogFilter;
+import hello.login.web.filter.LoginCheckFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,6 +20,16 @@ public class WebConfig {
         filterRegistrationBean.setFilter(new LogFilter()); // 등록할 필터를 지정하기
         filterRegistrationBean.setOrder(1); // 필터는 체인으로 동작하므로, 순서를 지정
         filterRegistrationBean.addUrlPatterns("/*"); // 필터를 적용할 url 패턴 지정
+
+        return filterRegistrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean loginCheckFilter() {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new LoginCheckFilter());
+        filterRegistrationBean.setOrder(2);
+        filterRegistrationBean.addUrlPatterns("/*");
 
         return filterRegistrationBean;
     }
