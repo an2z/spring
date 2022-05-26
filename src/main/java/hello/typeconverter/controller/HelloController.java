7dad54a9d5ab4f86bcf79036726f8ad2195aad18
yujeong lim <anneu5712@gmail.com>
2022-05-26 20:51:49 +0900

    타입 변환

---
+++ src/main/java/hello/typeconverter/controller/HelloController.java
@@ -0,0 +1,26 @@
+package hello.typeconverter.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+public class HelloController {
+
+    // 자바 타입 변환
+    @GetMapping("/hello-v1")
+    public String helloV1(HttpServletRequest request) {
+        String data = request.getParameter("data"); // 문자 타입 조회
+        Integer intValue = Integer.valueOf(data); // 문자 ➡ 숫자 타입으로 변경
+        System.out.println("intValue = " + intValue);
+        return "ok";
+    }
+
+    // 스프링 @RequestParam 타입 변환
+    @GetMapping("/hello-v2")
+    public String helloV2(@RequestParam Integer data) {
+        System.out.println("data = " + data);
+        return "ok";
+    }
+
+}
