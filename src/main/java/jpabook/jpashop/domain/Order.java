c08251a923cd6b27fa72fb1d126b197afc4926b6
yujeong lim <anneu5712@gmail.com>
2022-06-04 14:01:33 +0900

    ✒️Feat: 엔티티 클래스 개발

---
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,56 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static javax.persistence.FetchType.*;
+
+@Entity
+@Table(name = "orders")
+@Getter @Setter
+public class Order {
+
+    @Id @GeneratedValue
+    @Column(name = "order_id")
+    private Long id;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL) // 하나의 주문 정보는 여러개의 주문 상품을 가짐
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    @OneToOne(fetch = LAZY, cascade = CascadeType.ALL) // 하나의 주문 정보는 하나의 배송 정보를 가짐
+    @JoinColumn(name = "delivery_id")
+    private Delivery delivery;
+
+    private LocalDateTime orderDate; // 주문시간
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status; // 주문상태 [ORDER, CANCEL]
+
+    /**
+     * 연관관계 편의 메소드 ➡ 양방향 매핑에 사용
+    */
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
+
+}
