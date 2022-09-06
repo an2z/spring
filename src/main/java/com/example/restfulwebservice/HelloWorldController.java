de11c4d714813d15bac226f53467898e3e87e429
yujeong <b.imnuj@gmail.com>
2022-09-06 17:16:37 +0900

    feat : HelloWorld Bean 추가

--- src/main/java/com/example/restfulwebservice/HelloWorldController.java
+++ src/main/java/com/example/restfulwebservice/HelloWorldController.java
@@ -11,4 +11,12 @@ public class HelloWorldController {
         return "Hello World";
     }
 
+    /**
+     * 반환 타입이 자바 빈 형태일 경우 데이터를 Json 형태로 반환
+     */
+    @GetMapping("/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("Hello World");
+    }
+
 }
