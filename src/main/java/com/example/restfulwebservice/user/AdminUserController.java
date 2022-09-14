1563533131b8b944dd91f6b6222293f008ad6454
yujeong <b.imnuj@gmail.com>
2022-09-14 23:12:11 +0900

    feat : Response 데이터 제어를 위한 Filtering 처리 - SimpleBeanPropertyFilter 사용

--- src/main/java/com/example/restfulwebservice/user/AdminUserController.java
+++ src/main/java/com/example/restfulwebservice/user/AdminUserController.java
@@ -1,6 +1,9 @@
 package com.example.restfulwebservice.user;
 
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,19 +19,37 @@ public class AdminUserController {
     private final UserDaoService service;
 
     @GetMapping("/users")
-    public List<User> retrieveAllUsers() {
-        return service.findAll();
+    public MappingJacksonValue retrieveAllUsers() {
+        List<User> users = service.findAll();
+
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter
+                .filterOutAllExcept("id", "name", "joinDate", "password");
+
+        SimpleFilterProvider filters = new SimpleFilterProvider().addFilter("UserInfo", filter); // 필터 생성
+
+        MappingJacksonValue mapping = new MappingJacksonValue(users); // 데이터 변환
+        mapping.setFilters(filters); // 필터링 적용
+
+        return mapping;
     }
 
     @GetMapping("/users/{id}")
-    public User retrieveUser(@PathVariable int id) {
+    public MappingJacksonValue retrieveUser(@PathVariable int id) {
         User user = service.findOne(id);
 
         if (user == null) {
             throw new UserNotFoundException(String.format("ID[%S] not found", id));
         }
 
-        return user;
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter
+                .filterOutAllExcept("id", "name", "joinDate", "ssn");
+
+        SimpleFilterProvider filters = new SimpleFilterProvider().addFilter("UserInfo", filter); // 필터 생성
+
+        MappingJacksonValue mapping = new MappingJacksonValue(user); // 데이터 변환
+        mapping.setFilters(filters); // 필터링 적용
+
+        return mapping;
     }
 
 }
