3c17cca0a175575731d1f26944e1157203dacb92
yujeong <b.imnuj@gmail.com>
2022-09-14 22:27:03 +0900

    feat : AdminUser를 위한 컨트롤러 생성

---
+++ src/main/java/com/example/restfulwebservice/user/AdminUserController.java
@@ -0,0 +1,34 @@
+package com.example.restfulwebservice.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/admin")
+public class AdminUserController {
+
+    private final UserDaoService service;
+
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers() {
+        return service.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public User retrieveUser(@PathVariable int id) {
+        User user = service.findOne(id);
+
+        if (user == null) {
+            throw new UserNotFoundException(String.format("ID[%S] not found", id));
+        }
+
+        return user;
+    }
+
+}
