4f54153ac50ff760851f3fbbd35177536dbb279e
yujeong <b.imnuj@gmail.com>
2022-09-15 00:45:18 +0900

    refactor : MIME Type을 이용한 API 버전 관리

--- src/main/java/com/example/restfulwebservice/user/AdminUserController.java
+++ src/main/java/com/example/restfulwebservice/user/AdminUserController.java
@@ -34,7 +34,7 @@ public class AdminUserController {
         return mapping;
     }
 
-    @GetMapping(value = "/users/{id}", headers = "X-API-VERSION=1")
+    @GetMapping(value = "/users/{id}", produces = "application/vnd.company.appv1+json")
     public MappingJacksonValue retrieveUserV1(@PathVariable int id) {
         User user = service.findOne(id);
 
@@ -53,7 +53,7 @@ public class AdminUserController {
         return mapping;
     }
 
-    @GetMapping(value = "/users/{id}", headers = "X-API-VERSION=2")
+    @GetMapping(value = "/users/{id}", produces = "application/vnd.company.appv2+json")
     public MappingJacksonValue retrieveUserV2(@PathVariable int id) {
         User user = service.findOne(id);
 
