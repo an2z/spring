34d0304e2fb70beb9e2d699f27ad58bba60e69f3
yujeong <b.imnuj@gmail.com>
2022-09-16 16:16:17 +0900

    refactor : swqgger Documentation 커스텀 구현

--- src/main/java/com/example/restfulwebservice/user/User.java
+++ src/main/java/com/example/restfulwebservice/user/User.java
@@ -1,5 +1,7 @@
 package com.example.restfulwebservice.user;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,18 +14,23 @@ import java.util.Date;
 @NoArgsConstructor
 @AllArgsConstructor
 //@JsonFilter("UserInfo")
+@ApiModel(description = "사용자 상세 정보를 위한 도메인 객체")
 public class User {
 
     private Integer id;
 
     @Size(min = 2, message = "Name은 2글자 이상 입력해주세요.")
+    @ApiModelProperty(notes = "사용자 이름을 입력해주세요.")
     private String name;
 
     @Past
+    @ApiModelProperty(notes = "사용자 등록일을 입력해주세요.")
     private Date joinDate;
 
+    @ApiModelProperty(notes = "사용자 패스워드를 입력해주세요.")
     private String password;
 
+    @ApiModelProperty(notes = "사용자 주민번호를 입력해주세요.")
     private String ssn;
 
 }
