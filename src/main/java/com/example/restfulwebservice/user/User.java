4c88c3fb0d17544b540c987ef6beb92aff7f9e0b
yujeong lim <b.imnuj@gmail.com>
2022-09-21 23:21:52 +0900

    feat : User Entity에 Post와의 연관관계 추가

--- src/main/java/com/example/restfulwebservice/user/User.java
+++ src/main/java/com/example/restfulwebservice/user/User.java
@@ -6,13 +6,11 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
@@ -40,5 +38,8 @@ public class User {
 
     @ApiModelProperty(notes = "사용자 주민번호를 입력해주세요.")
     private String ssn;
+
+    @OneToMany(mappedBy = "user")
+    private List<Post> posts;
 
 }
