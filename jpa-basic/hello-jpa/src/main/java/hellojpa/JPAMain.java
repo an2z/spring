49ab0adafda16f4dea6ddca92e2cf5c6641be2a6
yujeong <b.imnuj@gmail.com>
2022-09-29 23:31:08 +0900

    ✒️Feat: 영속성 컨텍스트 플러시 강제 호출

--- jpa-basic/hello-jpa/src/main/java/hellojpa/JPAMain.java
+++ jpa-basic/hello-jpa/src/main/java/hellojpa/JPAMain.java
@@ -4,7 +4,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.util.List;
 
 public class JPAMain {
     public static void main(String[] args) {
@@ -17,10 +16,11 @@ public class JPAMain {
 
         try {
             // 영속
-            Member findMember1 = em.find(Member.class, 100L);
-            Member findMember2 = em.find(Member.class, 100L);
+            Member member = new Member(200L, "member200");
+            em.persist(member);
 
-            System.out.println("result = " + (findMember1 == findMember2)); // true 보장
+            em.flush(); // 플러시 강제 호출
+            System.out.println("==============");
 
             tx.commit();
         } catch (Exception e) {
