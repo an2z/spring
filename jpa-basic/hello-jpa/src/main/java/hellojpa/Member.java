ade840e05fec3037ecb42191b80ef3eaabcef14a
yujeong <b.imnuj@gmail.com>
2022-09-29 23:29:40 +0900

    ✒️Feat: 회원 엔티티 생성자 추가

--- jpa-basic/hello-jpa/src/main/java/hellojpa/Member.java
+++ jpa-basic/hello-jpa/src/main/java/hellojpa/Member.java
@@ -1,6 +1,8 @@
 package hellojpa;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Entity;
@@ -8,6 +10,8 @@ import javax.persistence.Id;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Member {
     @Id
     private Long id;
