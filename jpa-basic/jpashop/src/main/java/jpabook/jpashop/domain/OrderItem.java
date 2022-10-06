93aa314c2653498fc15ddb5001cb490a95291d71
yujeong <b.imnuj@gmail.com>
2022-10-06 23:21:31 +0900

    ♻️Refactor: OrderItem 엔티티에 연관관계 매핑 적용

--- jpa-basic/jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ jpa-basic/jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -2,10 +2,7 @@ package jpabook.jpashop.domain;
 
 import lombok.Getter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Getter
 @Entity
@@ -16,11 +13,13 @@ public class OrderItem {
     @Column(name = "order_item_id")
     private Long id;
 
-    @Column(name = "order_id")
-    private Long orderId;
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
 
-    @Column(name = "item_id")
-    private Long itemId;
+    @ManyToOne
+    @JoinColumn(name = "item_id")
+    private Item item;
 
     private int orderPrice;
 
