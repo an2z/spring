ce5185e786927e8aa8cad527b5564c2112ae1b1f
yujeong <b.imnuj@gmail.com>
2022-10-16 23:24:16 +0900

    ♻️Refactor: getter, setter 추가

--- jpa-basic/jpashop/src/main/java/jpabook/jpashop/domain/Book.java
+++ jpa-basic/jpashop/src/main/java/jpabook/jpashop/domain/Book.java
@@ -1,7 +1,12 @@
 package jpabook.jpashop.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 
+@Getter
+@Setter
 @Entity
 public class Book extends Item {
 
--- jpa-basic/jpashop/src/main/java/jpabook/jpashop/domain/Item.java
+++ jpa-basic/jpashop/src/main/java/jpabook/jpashop/domain/Item.java
@@ -1,12 +1,14 @@
 package jpabook.jpashop.domain;
 
 import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Getter
+@Setter
 @Entity
 @DiscriminatorColumn
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
