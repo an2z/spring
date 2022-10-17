112870321c8f381d5a88a35aeacc88283167c94b
yujeong <b.imnuj@gmail.com>
2022-10-17 23:21:13 +0900

    ♻️Refactor: 프록시 객체와 실제 객체 타입 비교 시 instanceof 사용

--- jpa-basic/hellojpa/src/main/java/hellojpa/JPAMain.java
+++ jpa-basic/hellojpa/src/main/java/hellojpa/JPAMain.java
@@ -16,21 +16,27 @@ public class JPAMain {
 
         try {
             // 저장
-            Member member = new Member();
-            member.setUsername("hello");
+            Member member1 = new Member();
+            member1.setUsername("member1");
+            em.persist(member1);
 
-            em.persist(member);
+            Member member2 = new Member();
+            member2.setUsername("member1");
+            em.persist(member2);
 
             em.flush();
             em.clear();
             
             // 조회
-//            Member findMember = em.find(Member.class, member.getId()); // 실제 엔티티 객체 조회
-            Member findMember = em.getReference(Member.class, member.getId()); // 프록시 엔티티 객체 조회
+            Member findMember = em.find(Member.class, member1.getId());
+            Member proxyMember = em.getReference(Member.class, member2.getId());
 
             System.out.println("findMember.class() = " + findMember.getClass());
-            System.out.println("findMember.id() = " + findMember.getId());
-            System.out.println("findMember.username() = " + findMember.getUsername());
+            System.out.println("proxyMember.class() = " + proxyMember.getClass());
+
+            // 타입 체크 시 == 비교 대신 instance of를 사용해야 한다.
+            System.out.println("findMember == proxyMember : " + (findMember == proxyMember));
+            System.out.println("findMember == proxyMember : " + (findMember instanceof Member || proxyMember instanceof Member));
 
             tx.commit();
         } catch (Exception e) {
