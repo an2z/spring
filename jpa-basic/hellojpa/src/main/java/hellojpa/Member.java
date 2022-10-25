06bdfe2d8690b0a55670919033d51f88f3ca882e
yujeong <b.imnuj@gmail.com>
2022-10-25 12:08:08 +0900

    ♻️Refactor: 값 타입 컬렉션 매핑 추가
    - @ElementCollection, @CollectionTable 사용

--- jpa-basic/hellojpa/src/main/java/hellojpa/Member.java
+++ jpa-basic/hellojpa/src/main/java/hellojpa/Member.java
@@ -6,6 +6,10 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Entity
 @Getter
@@ -27,4 +31,17 @@ public class Member {
 
     @Embedded
     private Address address;
+
+    @ElementCollection
+    @CollectionTable(name = "favorite_foods", joinColumns =
+    @JoinColumn(name = "member_id ")
+    )
+    @Column(name = "food_name")
+    private Set<String> favoriteFoods = new HashSet<>();
+
+    @ElementCollection
+    @CollectionTable(name = "address_history", joinColumns =
+    @JoinColumn(name = "member_id")
+    )
+    private List<Address> addressHistory = new ArrayList<>();
 }
