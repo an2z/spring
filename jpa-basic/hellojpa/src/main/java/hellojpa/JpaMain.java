0c55e0604f9ddbf8ef20ba7380d037c1ba3bce1b
yujeong lim <b.imnuj@gmail.com>
2022-10-28 23:55:01 +0900

    ♻️Refactor: Criteria 사용
    - Criteria는 JPA 공식 기능이지만, 실용성이 없어 실무에서는 잘 사용하지 않음

--- jpa-basic/hellojpa/src/main/java/hellojpa/JpaMain.java
+++ jpa-basic/hellojpa/src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,9 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.util.List;
 
 public class JpaMain {
@@ -16,12 +19,23 @@ public class JpaMain {
         tx.begin(); // 트랜잭션 시작
 
         try {
-            String jpql = "select m from Member as m where m.username like '%kim'";
-            List<Member> result = em.createQuery(jpql, Member.class).getResultList();
+            // Criteria
+            CriteriaBuilder cb = em.getCriteriaBuilder();
+            CriteriaQuery<Member> query = cb.createQuery(Member.class);
 
-            for (Member member : result) {
-                System.out.println("member = " + member);
+            Root<Member> m = query.from(Member.class);
+
+            // 자바코드로 쿼리 작성
+            CriteriaQuery<Member> cq = query.select(m).where(cb.equal(m.get("username"), "kim"));
+
+            // 동적 쿼리 추가
+            String username = "abs";
+            if (username != null) {
+                cq.where(cb.equal(m.get("username"), "kim"));
             }
+
+            List<Member> result = em.createQuery(cq)
+                    .getResultList();
 
             tx.commit();
         } catch (Exception e) {
