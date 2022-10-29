eff49ae5b95b5fe6563c685583622efe54639d59
yujeong lim <b.imnuj@gmail.com>
2022-10-29 15:04:36 +0900

    ♻️Refactor: 네이티브 SQL 사용

--- jpa-basic/hellojpa/src/main/java/hellojpa/JpaMain.java
+++ jpa-basic/hellojpa/src/main/java/hellojpa/JpaMain.java
@@ -4,10 +4,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
-import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -19,28 +15,13 @@ public class JpaMain {
         tx.begin(); // 트랜잭션 시작
 
         try {
-            // Criteria
-            CriteriaBuilder cb = em.getCriteriaBuilder();
-            CriteriaQuery<Member> query = cb.createQuery(Member.class);
-
-            Root<Member> m = query.from(Member.class);
-
-            // 자바코드로 쿼리 작성
-            CriteriaQuery<Member> cq = query.select(m).where(cb.equal(m.get("username"), "kim"));
-
-            // 동적 쿼리 추가
-            String username = "abs";
-            if (username != null) {
-                cq.where(cb.equal(m.get("username"), "kim"));
-            }
-
-            List<Member> result = em.createQuery(cq)
-                    .getResultList();
+            String sql = "select member_id, city, street, zipcode, user_name from Member";
+            em.createNativeQuery(sql, Member.class).getResultList();
 
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
-            System.out.println("e = " + e);
+            e.printStackTrace();
         } finally {
             em.close();
         }
