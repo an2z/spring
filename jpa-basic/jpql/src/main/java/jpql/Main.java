748d663983fc70cb498a19b0b4cf480df32d8f12
anneu <b.imnuj@gmail.com>
2022-11-05 23:24:08 +0900

    ♻️Refactor: 조인 대상 필터링을 위한 on절 사용
    - 회원과 팀을 조인, 이때 팀 이름이 teamA인 것만 조인

--- jpa-basic/jpql/src/main/java/jpql/Main.java
+++ jpa-basic/jpql/src/main/java/jpql/Main.java
@@ -28,9 +28,11 @@ public class Main {
             em.flush();
             em.clear();
 
-            String query = "select m from Member m, Team t where m.username = t.name";
+            String query = "select m from Member m left join m.team t on t.name = 'teamA'";
             List<Member> result = em.createQuery(query, Member.class)
                     .getResultList();
+
+            System.out.println("result = " + result);
 
             tx.commit();
         } catch (Exception e) {
