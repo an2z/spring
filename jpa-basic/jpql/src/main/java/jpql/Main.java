fca2e86bb1bf6e86192fc25b1c76a3c701eed364
anneu <b.imnuj@gmail.com>
2022-11-06 22:57:01 +0900

    ✒️Feat: 서브 쿼리 지원 함수 EXISTS 사용

--- jpa-basic/jpql/src/main/java/jpql/Main.java
+++ jpa-basic/jpql/src/main/java/jpql/Main.java
@@ -21,18 +21,34 @@ public class Main {
             member2.setUsername("member2");
             member2.setAge(20);
 
+            Member member3 = new Member();
+            member3.setUsername("member3");
+            member3.setAge(30);
+
             em.persist(member1);
             em.persist(member2);
+            em.persist(member3);
+
+            Team teamA = new Team();
+            teamA.setName("A");
+            teamA.addMember(member1);
+
+            Team teamB = new Team();
+            teamB.setName("B");
+            teamB.addMember(member2);
+
+            em.persist(teamA);
+            em.persist(teamB);
 
             em.flush();
             em.clear();
 
-            // 나이가 평균보다 많은 회원 조회
-            String query = "select m from Member m where m.age > (select avg(m2.age) from Member m2)";
-            List<Member> result = em.createQuery(query, Member.class)
-                    .getResultList();
+            /* 서브 쿼리 지원 함수 */
+            // 팀A 소속인 회원 조회 -> EXISTS
+            String query1 = "select m from Member m where exists (select t from m.team t where t.name = 'A')";
+            List<Member> result1 = em.createQuery(query1, Member.class).getResultList();
 
-            System.out.println("result = " + result);
+            System.out.println("result1 = " + result1);
 
             tx.commit();
         } catch (Exception e) {
