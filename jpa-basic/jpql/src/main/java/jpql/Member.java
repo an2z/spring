d8d708fbbfa1bad41ffc72116490513c2d0f9e08
anneu <b.imnuj@gmail.com>
2022-11-08 23:59:14 +0900

    ✒️Feat: MemberType 필드 추가

--- jpa-basic/jpql/src/main/java/jpql/Member.java
+++ jpa-basic/jpql/src/main/java/jpql/Member.java
@@ -1,6 +1,12 @@
 package jpql;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Member {
@@ -14,6 +20,8 @@ public class Member {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "team_id")
     private Team team;
+
+    private MemberType type;
 
     public Long getId() {
         return id;
@@ -45,6 +53,14 @@ public class Member {
 
     public void setTeam(Team team) {
         this.team = team;
+    }
+
+    public MemberType getType() {
+        return type;
+    }
+
+    public void setType(MemberType type) {
+        this.type = type;
     }
 
     @Override
