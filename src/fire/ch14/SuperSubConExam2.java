559ef0197da57724822bc1e0ba9444d5e6908904
anneu <b.imnuj@gmail.com>
2022-11-24 16:49:30 +0900

    클래스의 상속 (상속의 기본)

--- src/fire/ch14/SuperSubConExam2.java
+++ src/fire/ch14/SuperSubConExam2.java
@@ -1,57 +1,55 @@
 package src.fire.ch14;
 
-/* ���� Ŭ������ ������ ȣ��
- * super Ű���带 ����ؼ� ��������� ����Ŭ������ ȣ���� �� �ִ�.
- * super�� ������ �������� ��� ������ Ű�����̴�.
+
+/* 상위 클래스의 생성자 호출
+ * super 키워드를 사용해서 명시적으로 상위클래스를 호출할 수 있다.
+ * super는 생성자 내에서만 사용 가능한 키워드이다.
  */
-
-class SuperCls2 {
-	// ���� Ŭ���� ������ �����ε�
-	public SuperCls2() {
-		System.out.println("������ : SupeCls()");
-	}
-	
-	public SuperCls2(int i) {
-		System.out.println("������ : SupeCls(int i)");
-	}
-	
-	public SuperCls2(int i, int j) {
-		System.out.println("������ : SupeCls(int i, int j)");
-	}
-}
-
-
-class SubCls2 extends SuperCls2 {
-	// ���� Ŭ���� ������ �����ε�
-	public SubCls2() {
-		super(); // ���� Ŭ������ �����ڸ� ���� �� ȣ��
-		System.out.println("������ : SubCls()");
-	}
-	
-	public SubCls2(int i) {
-		super(i); // ���� Ŭ������ �����ڸ� ���� �� ȣ��
-		System.out.println("������ : SubCls(int i)");
-	}
-	
-	public SubCls2(int i, int j) {
-		super(i, j); // ���� Ŭ������ �����ڸ� ���� �� ȣ��
-		System.out.println("������ : SubCls(int i, int j)");
-	}
-}
-
-
 class SuperSubConExam2 {
-	public static void main(String[] args) {
-		System.out.println("1. ");
-		new SubCls2();
-		System.out.println();
-		
-		System.out.println("2. ");
-		new SubCls2(1);
-		System.out.println();
-		
-		System.out.println("3. ");
-		new SubCls2(1, 2);
-		System.out.println();
-	}
+    public static void main(String[] args) {
+        System.out.println("1. ");
+        new SubCls();
+        System.out.println();
+
+        System.out.println("2. ");
+        new SubCls(1);
+        System.out.println();
+
+        System.out.println("3. ");
+        new SubCls(1, 2);
+        System.out.println();
+    }
+
+    static class SuperCls {
+        // 상위 클래스 생성자 오버로딩
+        public SuperCls() {
+            System.out.println("생성자 : SuperCls()");
+        }
+
+        public SuperCls(int i) {
+            System.out.println("생성자 : SuperCls(int i)");
+        }
+
+        public SuperCls(int i, int j) {
+            System.out.println("생성자 : SupersCls(int i, int j)");
+        }
+    }
+
+    static class SubCls extends SuperCls {
+        // 하위 클래스 생성자 오버로딩
+        public SubCls() {
+            super(); // 상위 클래스의 생성자를 지정 및 호출
+            System.out.println("생성자 : SubCls()");
+        }
+
+        public SubCls(int i) {
+            super(i); // 상위 클래스의 생성자를 지정 및 호출
+            System.out.println("생성자 : SubCls(int i)");
+        }
+
+        public SubCls(int i, int j) {
+            super(i, j); // 상위 클래스의 생성자를 지정 및 호출
+            System.out.println("생성자 : SubCls(int i, int j)");
+        }
+    }
 }
