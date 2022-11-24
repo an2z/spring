ca86e7d5dccceb48d1e2f980d33ad97ee6ae9376
anneu <b.imnuj@gmail.com>
2022-11-24 14:51:26 +0900

    조건문, 반복문

--- src/fire/ch05/IfExam.java
+++ src/fire/ch05/IfExam.java
@@ -1,75 +1,76 @@
 package src.fire.ch05;
 
 class IfExam {
-	public static void main(String[] args) {
-		int n1 = 5;
-		int n2 = 7;
-		
-		/* ���ǹ� (if) */
-		if(n1 < n2) {
-			System.out.println("n1 < n2 is true");
-		}		
-		if (n1 < n2)  // �߰�ȣ ���� ����
-			System.out.println("n1 < n2 is true");
-		
-		/* ���ǹ� (if ~ else) */
-		if (n1 == n2) {
-			System.out.println("n1 == n2 is true");
-		} else {
-			System.out.println("n1 == n2 is false");
-		}
-		
-		/* ���ǹ� (if ~ else if ~ else) */
-		if (n1 > n2)                                // ���� �˻�
-			System.out.println("n1 > n2 is true");
-		else if (n1 == n2)                          // �� ������ �������� ������ ���⼭ �ٽ� �˻�
-			System.out.println("n1 == n2 is true");
-		else                                        // �ƹ��͵� �������� ������ else ����
-			System.out.println("n2 < n1 is true");
-		
-		
-		/* ���� ������ (���� ������) */
-		int num1 = 50;
-		int num2 = 100;
-		int big;
-		int diff;
-		
-		big = (num1 > num2) ? num1 : num2;
-		System.out.println("ū ��: " + big);
-		
-		diff = (num1 > num2) ? (num1 - num2) : (num2 - num1);
-		System.out.println("���밪: " + diff);
-		
-		
-		/* switch�� */
-		int n = 3;
-		
-		switch(n) {  // case -> ���̺�(å����) ����
-		case 1:
-			System.out.println("simple java");
-		case 2:
-			System.out.println("funny java");
-		case 3:
-			System.out.println("fantastic java");
-		default:
-			System.out.println("the best programming language");
-		}
-		
-		// switch + break��
-		switch(n) {
-		case 1: 
-			System.out.println("simple java");
-			break;
-		case 2:
-			System.out.println("funny java");
-			break;
-		case 3:
-			System.out.println("fantastic java");
-			break;
-		default:
-			System.out.println("the best programming language");
-		}
-		System.out.println("do you like java");
-	}
+    public static void main(String[] args) {
+        int n1 = 5;
+        int n2 = 7;
 
+        /* 조건문 (if) */
+        if (n1 < n2) {
+            System.out.println("n1 < n2 is true");
+        }
+        if (n1 < n2) {
+            System.out.println("n1 < n2 is true"); // 중괄호 생략 가능
+        }
+
+        /* 조건문 (if ~ else) */
+        if (n1 == n2) {
+            System.out.println("n1 == n2 is true");
+        } else {
+            System.out.println("n1 == n2 is false");
+        }
+
+        /* 조건문 (if ~ else if ~ else) */
+        if (n1 > n2) {                              // 먼저 검사
+            System.out.println("n1 > n2 is true");
+        } else if (n1 == n2) {                      // 위 조건이 만족되지 않으면 여기서 다시 검사
+            System.out.println("n1 == n2 is true");
+        } else {                                    // 아무것도 만족하지 않으면 else 실행
+            System.out.println("n2 < n1 is true");
+        }
+
+
+        /* 조건 연산자 (삼항 연산자) */
+        int num1 = 50;
+        int num2 = 100;
+        int big;
+        int diff;
+
+        big = (num1 > num2) ? num1 : num2;
+        System.out.println("큰 수: " + big);
+
+        diff = (num1 > num2) ? (num1 - num2) : (num2 - num1);
+        System.out.println("절대값: " + diff);
+
+
+        /* switch문 */
+        int n = 3;
+
+        switch (n) { // case -> 레이블(책갈피) 역할
+            case 1:
+                System.out.println("simple java");
+            case 2:
+                System.out.println("funny java");
+            case 3:
+                System.out.println("fantastic java");
+            default:
+                System.out.println("the best programming language");
+        }
+
+        // switch + break문
+        switch (n) {
+            case 1:
+                System.out.println("simple java");
+                break;
+            case 2:
+                System.out.println("funny java");
+                break;
+            case 3:
+                System.out.println("fantastic java");
+                break;
+            default:
+                System.out.println("the best programming language");
+        }
+        System.out.println("do you like java");
+    }
 }
