1c309e8b0087d24f9ebe47c2d56121c9484e122f
anneu <b.imnuj@gmail.com>
2022-11-24 16:33:45 +0900

    향상된 for문

--- src/fire/ch13/ForEachExam.java
+++ src/fire/ch13/ForEachExam.java
@@ -2,22 +2,21 @@ package src.fire.ch13;
 
 /* EnhancedFor */
 class ForEachExam {
-	public static void main(String[] args) {
-		int[] arr = {1, 2, 3, 4, 5};
-		
-		// �迭 ��� ��ü ���
-		for (int e : arr) {
-			System.out.println(e + "");
-		}
-		System.out.println();  // �ܼ� �� �ٲ�
-		
-		int sum = 0;
-		
-		// �迭 ����� ��ü �� ���
-		for (int e : arr) {
-			sum += e;
-		}
-		System.out.println("sum: " + sum);
-	}
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
 
+        // 배열 요소 전체 출력
+        for (int e : arr) {
+            System.out.print(e + " ");
+        }
+        System.out.println();
+
+        int sum = 0;
+
+        // 배열 요소의 전체 합 출력
+        for (int e : arr) {
+            sum += e;
+        }
+        System.out.println("sum: " + sum);
+    }
 }
--- src/fire/ch13/ForEachInstExam.java
+++ src/fire/ch13/ForEachInstExam.java
@@ -2,39 +2,38 @@ package src.fire.ch13;
 
 
 class ForEachInstExam {
-	public static void main(String[] args) {
-		Box2[] arr = new Box2[5];
-		arr[0] = new Box2(101, "One");
-		arr[1] = new Box2(202, "Two");
-		arr[2] = new Box2(303, "Three");
-		arr[3] = new Box2(404, "Four");
-		arr[4] = new Box2(505, "Five");
-		
-		// �迭���� ��ȣ�� 505�� Box�� ã�� �� ���빰�� ����ϴ� �ݺ���
-		for (Box2 e : arr) {
-			if (e.getBoxNum() == 505)
-				System.out.println(e);
-		}
-	
-	}
-}
+    public static void main(String[] args) {
+        Box2[] arr = new Box2[5];
 
+        arr[0] = new Box2(101, "One");
+        arr[1] = new Box2(202, "Two");
+        arr[2] = new Box2(303, "Three");
+        arr[3] = new Box2(404, "Four");
+        arr[4] = new Box2(505, "Five");
 
-class Box2 {
-	private String contents;
-	private int boxNum;
-	
-	Box2(int num, String cont) {
-		boxNum = num;
-		contents = cont;
-	}
-	
-	public int getBoxNum() {
-		return boxNum;
-	}
-	
-	public String toString() {
-		return contents;
-	}
-	
+        // 배열에서 번호가 505인 Box를 찾아 그 내용물을 출력하는 반복문
+        for (Box2 a : arr) {
+            if (a.getBoxNum() == 505) {
+                System.out.println(a);
+            }
+        }
+    }
+
+    static class Box2 {
+        private String contents;
+        private int boxNum;
+
+        Box2(int num, String cont) {
+            boxNum = num;
+            contents = cont;
+        }
+
+        public int getBoxNum() {
+            return boxNum;
+        }
+
+        public String toString() {
+            return contents;
+        }
+    }
 }
