74569c02f490902249aab2981acbb778384be3b7
anneu <b.imnuj@gmail.com>
2022-11-24 14:46:36 +0900

    상수, 형변환

--- src/fire/ch03/ConversionExam.java
+++ src/fire/ch03/ConversionExam.java
@@ -1,16 +1,14 @@
 package src.fire.ch03;
 
 class ConversionExam {
-	public static void main(String[] args) {
-		/* �ڵ� �� ��ȯ */
-		int num1 = 50;
-		long num2 = 3147483647l;
-		System.out.println(num1 + num2);  // �� �ǿ������� �ڷ����� �ٸ�
-		
-		
-		/* ����� �� ��ȯ */
-		double pi = 3.1415;
-		int wholenumber = (int)pi;  // pi ���� int������ ����� �� ��ȯ
-	}
+    public static void main(String[] args) {
+        /* 자동 형 변환 */
+        int num1 = 50;
+        long num2 = 3147483647L;
+        System.out.println(num1 + num2);  // 두 피연산자의 자료형이 다름
 
+        /* 명시적 형 변환 */
+        double pi = 3.1415;
+        int wholeNumber = (int) pi;  // pi 값을 int형으로 명시적 형 변환
+    }
 }
