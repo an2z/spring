e25938b8ef6c2b98a8887b473a95f34c0029e1a5
anneu <b.imnuj@gmail.com>
2022-11-24 14:38:34 +0900

    변수, 자료형

--- src/fire/ch02/IntegerExam.java
+++ src/fire/ch02/IntegerExam.java
@@ -1,15 +1,14 @@
 package src.fire.ch02;
 
-/* ���� �ڷ��� */
+/* 정수 자료형 */
 public class IntegerExam {
-	public static void main(String[] args) {		
-		int num1;                // ���� num1 ����
-		num1 = 10;               // ���� num1�� ���� 10�� ����	
-		
-		int num2 = 20;           // ���� num2 ����� ���ÿ� 20���� �ʱ�ȭ
-		
-		int num3 = num1 + num2;  // �� ���� ���� ������� ����
-		System.out.println(num1 + "+" + num2 + "=" + num3);
-	}
-	
+    public static void main(String[] args) {
+        int num1;                // 변수 num1 선언
+        num1 = 10;               // 변수 num1에 정수 10을 저장
+
+        int num2 = 20;           // 변수 num2 선언과 동시에 20으로 초기화
+
+        int num3 = num1 + num2;  // 두 변수 값을 대상으로 덧셈
+        System.out.println(num1 + "+" + num2 + "=" + num3);
+    }
 }
