af404acc3cc221ff50a2ba1c7d08817b88f2703a
anneu <b.imnuj@gmail.com>
2022-11-24 15:22:33 +0900

    정보은닉, 캡슐화

--- src/fire/ch09/Cap.java
+++ src/fire/ch09/Cap.java
@@ -1,38 +1,39 @@
 package src.fire.ch09;
 
-/* ĸ��ȭ */
+/* 캡슐화 */
 public class Cap {
-	public static void main(String[] args) {
-		ColdPatient patient = new ColdPatient();  // ȯ�� ����		
-		patient.coldTakeList(new ColdCap());
-		
-	}
-}
+    public static void main(String[] args) {
+        ColdPatient patient = new ColdPatient(); // 환자 생성
+        patient.coldTakeList(new ColdCap());
+    }
 
+    static class ColdCap {
+        // 콧물 처치
+        void snot() {
+            System.out.println("콧물이 싹~ 납니다.");
+        }
 
-class ColdCap {
-	void snot() {    // �๰ óġ
-		System.out.println("�๰�� ��~ ���ϴ�.");
-	}
-	
-	void sneeze() {  // ��ä�� óġ
-		System.out.println("��ä�Ⱑ �ܽ��ϴ�.");
-	}
-	
-	void snuffle() {    // �� ���� óġ
-		System.out.println("�ڰ� �� �ո��ϴ�.");
-	}
-	
-	void coldTake() {  // ���� ���� ��� �� ������ ��� �޼ҵ�
-		snot();
-		sneeze();
-		snuffle();
-	}
-}
+        // 재채기 처치
+        void sneeze() {
+            System.out.println("재채기가 멎습니다.");
+        }
 
+        // 코 막힘 처치
+        void snuffle() {
+            System.out.println("코가 뻥 뚫립니다.");
+        }
 
-class ColdPatient {
-	void coldTakeList(ColdCap cap) {
-		cap.coldTake();
-	}
+        // 약의 복용 방법 및 순서가 담긴 메소드
+        void coldTake() {
+            snot();
+            sneeze();
+            snuffle();
+        }
+    }
+
+    static class ColdPatient {
+        void coldTakeList(ColdCap cap) {
+            cap.coldTake();
+        }
+    }
 }
