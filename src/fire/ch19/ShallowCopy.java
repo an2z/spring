4b206cb7312e67ae254050419f5e0e66333eb351
anneu <b.imnuj@gmail.com>
2022-11-25 14:21:45 +0900

    Object 클래스의 메소드

--- src/fire/ch19/ShallowCopy.java
+++ src/fire/ch19/ShallowCopy.java
@@ -1,82 +1,79 @@
 package src.fire.ch19;
 
-/* ���� ���� (Shallow Copy)
- * �ּ� ���� �����Ѵٴ� �ǹ� 
- * �����ϰ� �ִ� �������� ����
+/* 얕은 복사 (Shallow Copy)
+ * 주소 값을 복사한다는 의미
+ * 참조하고 있는 실제값이 같음
  */
-
-class Point2 implements Cloneable {
-	private int xPos;
-	private int yPos;
-	
-	public Point2 (int x, int y) {
-		xPos = x;
-		yPos = y;
-	}
-	
-	public void showPostion() {
-		System.out.printf("[%d, %d]", xPos, yPos);
-		System.out.println();
-	}
-	
-	public void changePos(int x, int y) {
-		xPos = x;
-		yPos = y;
-	}
-	
-	@Override
-	public Object clone() throws CloneNotSupportedException {
-		return super.clone();
-	}
-}
-
-
-class Rectangle2 implements Cloneable {
-	private Point2 upperLeft;	// ���� ��� ��ǥ
-	private Point2 lowerRight;	// ���� �ϴ� ��ǥ
-	
-	public Rectangle2 (int x1, int y1, int x2, int y2) {
-		upperLeft = new Point2(x1, y1);
-		lowerRight = new Point2(x2, y2);
-	}
-	
-	public void changePos(int x1, int y1, int x2, int y2) {		// ��ǥ ���� ����
-		upperLeft.changePos(x1, y1);
-		lowerRight.changePos(x2, y2);
-	}
-	
-	@Override
-	public Object clone() throws CloneNotSupportedException {
-		return super.clone();
-	}
-	
-	public void showPosition() {	// ���簢�� ��ǥ ���� ���
-		System.out.print("���� ��� : ");
-		upperLeft.showPostion();
-		
-		System.out.print("���� �ϴ� : ");
-		lowerRight.showPostion();
-	}	
-}
-
-
 class ShallowCopy {
-	public static void main(String[] args) {
-		Rectangle2 org = new Rectangle2(1, 1, 9, 9);
-		Rectangle2 cpy;
-		
-		try {
-			cpy = (Rectangle2)org.clone();	// �ν��Ͻ� ����
-			org.changePos(2, 2, 7, 7);		// �ν��Ͻ� ��ǥ ���� ����
-			
-			System.out.println("---- org ��ǥ ----");
-			org.showPosition();
-			
-			System.out.println("---- cpy ��ǥ ----");
-			cpy.showPosition();
-		}
-		catch(CloneNotSupportedException e) {
-			e.printStackTrace();
-		}
-	}
+    public static void main(String[] args) {
+        Rectangle org = new Rectangle(1, 1, 9, 9);
+        Rectangle cpy;
+
+        try {
+            cpy = (Rectangle) org.clone(); // 인스턴스 복사
+            org.changePos(2, 2, 7, 7); // 인스턴스 좌표 정보 수정
+
+            System.out.println("---- org 좌표 ----");
+            org.showPosition();
+
+            System.out.println("---- cpy 좌표 ----");
+            cpy.showPosition();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static class Point implements Cloneable {
+        private int xPos;
+        private int yPos;
+
+        public Point(int x, int y) {
+            xPos = x;
+            yPos = y;
+        }
+
+        public void showPosition() {
+            System.out.printf("[%d, %d]", xPos, yPos);
+            System.out.println();
+        }
+
+        public void changePos(int x, int y) {
+            xPos = x;
+            yPos = y;
+        }
+
+        @Override
+        public Object clone() throws CloneNotSupportedException {
+            return super.clone();
+        }
+    }
+
+
+    static class Rectangle implements Cloneable {
+        private Point upperLeft; // 좌측 상단 좌표
+        private Point lowerRight; // 우측 하단 좌표
+
+        public Rectangle(int x1, int y1, int x2, int y2) {
+            upperLeft = new Point(x1, y1);
+            lowerRight = new Point(x2, y2);
+        }
+
+        public void changePos(int x1, int y1, int x2, int y2) { // 좌표 정보 수정
+            upperLeft.changePos(x1, y1);
+            lowerRight.changePos(x2, y2);
+        }
+
+        @Override
+        public Object clone() throws CloneNotSupportedException {
+            return super.clone();
+        }
+
+        public void showPosition() { // 직사각형 좌표 정보 출력
+            System.out.print("좌측 상단 : ");
+            upperLeft.showPosition();
+
+            System.out.print("우측 하단 : ");
+            lowerRight.showPosition();
+        }
+    }
 }
--- src/fire/ch19/StringEqualityMethod.java
+++ src/fire/ch19/StringEqualityMethod.java
@@ -1,25 +1,26 @@
 package src.fire.ch19;
 
-/* String Ŭ������ Equals �޼ҵ�
- * ==�����ڴ� ���������� �������� ��
- * equals�޼ҵ�� �ν��Ͻ��� ������ �� 
+/* String 클래스의 Equals 메소드
+ * == 연산자: 참조변수의 참조값 비교
+ * equals(): 인스턴스의 내용 비교
  */
-
 class StringEqualityMethod {
-	public static void main(String[] args) {
-		String str1 = new String("Simple");
-		String str2 = new String("Simple");
-		
-		// ���� ���� ��
-		if (str1 == str2)
-			System.out.println("str1, str2 ���� ���� ����");
-		else
-			System.out.println("str1, str2 ���� ���� �ٸ�");
-		
-		// �� �ν��Ͻ��� ������ ��
-		if (str1.equals(str2))
-			System.out.println("str1, str2 ������ ����");
-		else
-			System.out.println("str1, str2 ������ �ٸ�");
-	}
+    public static void main(String[] args) {
+        String str1 = new String("Simple");
+        String str2 = new String("Simple");
+
+        // 참조 값을 비교
+        if (str1 == str2) {
+            System.out.println("str1, str2 참조 값이 동일");
+        } else {
+            System.out.println("str1, str2 참조 값이 다름");
+        }
+
+        // 두 인스턴스의 내용을 비교
+        if (str1.equals(str2)) {
+            System.out.println("str1, str2 내용이 동일");
+        } else {
+            System.out.println("str1, str2 내용이 다름");
+        }
+    }
 }
