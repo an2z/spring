dc476763808afbb6286ad0b7f2097ea573f6ba57
anneu <b.imnuj@gmail.com>
2022-11-30 14:51:52 +0900

    feat: 관람객을 입장시키는 기능 구현

--- oop/src/objects/tiketsales/Theatar.java
+++ oop/src/objects/tiketsales/Theatar.java
@@ -9,4 +9,19 @@ public class Theatar {
     public Theatar(TicketSeller ticketSeller) {
         this.ticketSeller = ticketSeller;
     }
+
+    /**
+     * 관람객 입장
+     */
+    public void enter(Audience audience) {
+        if (audience.getBag().hasInvitation()) {
+            Ticket ticket = ticketSeller.getTicketOffice().getTicket();
+            audience.getBag().setTicket(ticket);
+        } else {
+            Ticket ticket = ticketSeller.getTicketOffice().getTicket();
+            audience.getBag().minusMoney(ticket.getFee());
+            ticketSeller.getTicketOffice().minusMoney(ticket.getFee());
+            audience.getBag().setTicket(ticket);
+        }
+    }
 }
