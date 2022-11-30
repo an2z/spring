4bb19e7fa970c34684124a5be22b823a27278221
anneu <b.imnuj@gmail.com>
2022-11-30 14:51:16 +0900

    feat: 소극장 클래스 생성

---
+++ oop/src/objects/tiketsales/Theatar.java
@@ -0,0 +1,12 @@
+package objects.tiketsales;
+
+/**
+ * 소극장
+ */
+public class Theatar {
+    private TicketSeller ticketSeller;
+
+    public Theatar(TicketSeller ticketSeller) {
+        this.ticketSeller = ticketSeller;
+    }
+}
