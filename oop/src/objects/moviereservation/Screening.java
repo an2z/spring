d7066ed33a91f5c38c989156148488f8fb8bd972
anneu <b.imnuj@gmail.com>
2022-12-04 12:00:46 +0900

    feat: 상영 클래스 생성
    인스턴스 변수의 접근제어자는 private, 메서드는 public으로 두어
    외부에서는 객체의 속성에 직접 접근할 수 없도록 막고, 적절한 메서드를 통해서만 내부 상태를 변경할 수 있게한다.

--- oop/src/objects/moviereservation/Screening.java
+++ oop/src/objects/moviereservation/Screening.java
@@ -1,2 +1,31 @@
-package objects.moviereservation;public class Screening {
+package objects.moviereservation;
+
+import java.time.LocalDate;
+
+/**
+ * 상영
+ */
+public class Screening {
+    private Movie movie;
+    private int sequence;
+    private LocalDate whenScreened;
+
+    public Screening(Movie movie, int sequence, LocalDate whenScreened) {
+        this.movie = movie;
+        this.sequence = sequence;
+        this.whenScreened = whenScreened;
+    }
+
+
+    public boolean isSequence(int sequence) {
+        return this.sequence == sequence;
+    }
+
+    public LocalDate getStartTime() {
+        return whenScreened;
+    }
+
+    public Money getMovieFee() {
+        return movie.getFee();
+    }
 }
