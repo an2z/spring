e81bbc3069bd66425ac53c6b9f351d4a24b230ca
anneu <b.imnuj@gmail.com>
2022-12-07 18:45:33 +0900

    feat: 영화의 할인 정책을 변경할 수 있는 메서드 추가

--- oop/src/objects/moviereservation/Movie.java
+++ oop/src/objects/moviereservation/Movie.java
@@ -25,4 +25,8 @@ public class Movie {
     public Money calculateMovieFee(Screening screening) {
         return fee.minus(discountPolicy.calculateDiscountAmount(screening)); // 기본 요금에서 할인 요금 차감
     }
+
+    public void changeDiscountPolicy(DiscountPolicy discountPolicy) {
+        this.discountPolicy = discountPolicy;
+    }
 }
