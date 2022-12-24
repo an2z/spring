5d9a2eebb90a42b61532c25624f884623be74308
anneu <b.imnuj@gmail.com>
2022-12-24 16:14:55 +0900

    fix: 무한루프 문제 해결을 위한 JsonIgnore 추가
    양방향 매핑이라면 한쪽은 JsonIgnore 해줘야 무한루프가 발생하지 않는다.

--- jpa-shop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ jpa-shop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jpabook.jpashop.domain.item.Item;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -23,6 +24,7 @@ public class OrderItem {
     @JoinColumn(name = "item_id")
     private Item item;
 
+    @JsonIgnore
     @ManyToOne(fetch = LAZY) // 여러개의 주문 상품은 하나의 주문 정보를 가짐
     @JoinColumn(name = "order_id")
     private Order order;
