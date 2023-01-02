e624f8f587aabb8ce22785686c4e062c41c99200
anneu <b.imnuj@gmail.com>
2023-01-02 15:49:01 +0900

    feat: 고정 할인 정책 구현

---
+++ basic/src/main/java/core/basic/discount/DiscountPolicy.java
@@ -0,0 +1,7 @@
+package core.basic.discount;
+
+import core.basic.member.Member;
+
+public interface DiscountPolicy {
+    int discount(Member member, int price);
+}
---
+++ basic/src/main/java/core/basic/discount/FixDiscountPolicy.java
@@ -0,0 +1,16 @@
+package core.basic.discount;
+
+import core.basic.member.Grade;
+import core.basic.member.Member;
+
+public class FixDiscountPolicy implements DiscountPolicy {
+    private int discountAmount = 1000; // 천원 고정 할인
+
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP) {
+            return discountAmount;
+        }
+        return 0;
+    }
+}
