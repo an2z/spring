d6f396d955d5f3f59d8684b7779f8e83951cc1e7
anneu <b.imnuj@gmail.com>
2023-01-03 23:11:12 +0900

    test: Configuration 어노테이션이 있는 경우 싱글톤 보장 테스트

---
+++ basic/src/test/java/core/basic/singleton/ConfigurationSingletonTest.java
@@ -0,0 +1,41 @@
+package core.basic.singleton;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import core.basic.AppConfig;
+import core.basic.member.MemberRepository;
+import core.basic.member.MemberServiceImpl;
+import core.basic.order.OrderServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+class ConfigurationSingletonTest {
+
+    @Test
+    void configurationTest() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);
+        MemberRepository memberRepository = ac.getBean("memberRepository", MemberRepository.class);
+
+        MemberRepository memberRepository1 = memberService.getMemberRepository();
+        MemberRepository memberRepository2 = orderService.getMemberRepository();
+
+        // 모두 같은 객체임을 확인
+        System.out.println("memberService -> memberRepository = " + memberRepository1);
+        System.out.println("orderService -> memberRepository = " + memberRepository2);
+        System.out.println("memberRepository = " + memberRepository);
+
+        assertThat(memberRepository1).isSameAs(memberRepository);
+        assertThat(memberRepository2).isSameAs(memberRepository);
+    }
+
+    @Test
+    void configurationDepp() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        AppConfig bean = ac.getBean(AppConfig.class);
+        System.out.println("bean = " + bean); // CGLIB
+    }
+}
