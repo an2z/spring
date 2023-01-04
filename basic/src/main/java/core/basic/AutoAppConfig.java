c9e967c8028b0618fd195b455d4ea5d4f0201e5c
anneu <b.imnuj@gmail.com>
2023-01-04 22:57:19 +0900

    test: 스프링 빈 중복 등록 시 충돌 테스트
    컴포넌트 스캔에 의한 자동 빈 등록에 중복 이름이 있을 경우 ConflictingBeanDefinitionException 오류가 발생한다.
    
    자동 빈 등록과 수동 빈 등록에 중복 이름이 있을 경우 수동 빈이 더욱 우선권을 갖지만, 복잡한 버그 발생 위험이 있어 스프링 부트에서 오류를 발생시킨다.

--- basic/src/main/java/core/basic/AutoAppConfig.java
+++ basic/src/main/java/core/basic/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package core.basic;
 
+import core.basic.member.MemberRepository;
+import core.basic.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -9,5 +12,8 @@ import org.springframework.context.annotation.FilterType;
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 ) // 기존 AppConfig 예제를 유지하기 위한 설정
 public class AutoAppConfig {
-
+    @Bean(name = "memoryMemberRepository")
+    MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
 }
