ee4d4a54c09fa6342836e5825595d2395946fd4b
anneu <b.imnuj@gmail.com>
2023-01-05 16:55:55 +0900

    refactor: 자동 의존관계 주입 시 타입 외 추가 매칭에 사용될 파라미터 이름 지정
    @Autowired는 먼저 타입으로 매칭을 진행하고, 그 결과 여러개의 빈이 존재하면 필드나 파라미터 이름으로 추가 매칭한다.

--- basic/src/main/java/core/basic/order/OrderServiceImpl.java
+++ basic/src/main/java/core/basic/order/OrderServiceImpl.java
@@ -3,14 +3,19 @@ package core.basic.order;
 import core.basic.discount.DiscountPolicy;
 import core.basic.member.Member;
 import core.basic.member.MemberRepository;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
+
+    @Autowired
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy rateDiscountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = rateDiscountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
