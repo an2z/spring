931806121e413c3108312bd4992d6e0ec60f621c
anneu <b.imnuj@gmail.com>
2023-01-05 17:47:28 +0900

    style: 변수명 오탈자 수정

--- basic/src/main/java/core/basic/discount/RateDiscountPolicy.java
+++ basic/src/main/java/core/basic/discount/RateDiscountPolicy.java
@@ -8,12 +8,12 @@ import org.springframework.stereotype.Component;
 @Component
 @MainDiscountPolicy
 public class RateDiscountPolicy implements DiscountPolicy {
-    private int didscountAmount = 10; // 10% 할인
+    private int discountAmount = 10; // 10% 할인
 
     @Override
     public int discount(Member member, int price) {
         if (member.getGrade() == Grade.VIP) {
-            return price * didscountAmount / 100;
+            return price * discountAmount / 100;
         }
         return 0;
     }
