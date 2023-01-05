1f6ab2e05692bf1fc724e0705590c4d905e0bfe0
anneu <b.imnuj@gmail.com>
2023-01-05 14:57:02 +0900

    refactor: 롬복을 사용한 생성자 주입 적용
    @RequiredArgsConstructor 롬복을 사용하면 final 키워드가 붙은 필드를 모아 자동으로 생성자를 만든다.
    
    생성자를 하나만 둘 경우 @Autowired 생략 또한 가능하기 때문에 롬복 활용시 코드가 훨씬 간결해진다.

--- basic/src/main/java/core/basic/order/OrderServiceImpl.java
+++ basic/src/main/java/core/basic/order/OrderServiceImpl.java
@@ -3,19 +3,14 @@ package core.basic.order;
 import core.basic.discount.DiscountPolicy;
 import core.basic.member.Member;
 import core.basic.member.MemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
-
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
