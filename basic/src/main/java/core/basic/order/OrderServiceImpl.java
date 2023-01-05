c29e62b4e498fb002a2781b42dff1e4620b26010
anneu <b.imnuj@gmail.com>
2023-01-05 13:40:08 +0900

    feat: 생성자를 통한 의존관계 자동 주입
    불변, 필수 의존관계에 생성자 주입을 사용
    
    필드에 private final 키워드를 넣어 값을 필수적으로 가지도록 보장하고, 변경되지 않도록 한다.
    
    생성자가 단 1개라면 @Autowired를 생략 가능

--- basic/src/main/java/core/basic/order/OrderServiceImpl.java
+++ basic/src/main/java/core/basic/order/OrderServiceImpl.java
@@ -8,10 +8,14 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class OrderServiceImpl implements OrderService {
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
+
     @Autowired
-    private MemberRepository memberRepository;
-    @Autowired
-    private DiscountPolicy discountPolicy;
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
