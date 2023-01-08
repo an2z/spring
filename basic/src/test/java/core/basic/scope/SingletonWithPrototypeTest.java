4c2ec97e33ac22854fe8c69f5ab66db4ccebe108
anneu <b.imnuj@gmail.com>
2023-01-08 15:09:19 +0900

    feat: 싱글톤 + 프로토타입 빈 함께 사용
    싱글톤 빈을 만들고 그 안에서 프로토타입 빈을 주입 받으면, 프로토타입 빈이 싱글톤 빈에 종속되어 스프링 컨테이너에서 소멸되지 않고 해당 프로토타입 인스턴스를 가지고 있음
    
    따라서 프로토타입으로 항상 새로운 빈이 생성되지 않고, 싱글톤 빈 안에서 동일한 프로토타입 빈이 반환된다.

--- basic/src/test/java/core/basic/scope/SingletonWithPrototypeTest.java
+++ basic/src/test/java/core/basic/scope/SingletonWithPrototypeTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -21,6 +22,31 @@ class SingletonWithPrototypeTest {
 
         assertThat(prototypeBean1.getCount()).isEqualTo(1);
         assertThat(prototypeBean2.getCount()).isEqualTo(1);
+    }
+
+    @Test
+    void singletonClientUsePrototype() {
+        ApplicationContext ac =
+                new AnnotationConfigApplicationContext(ClientBean.class, PrototypeBean.class);
+
+        ClientBean clientBean1 = ac.getBean(ClientBean.class);
+        ClientBean clientBean2 = ac.getBean(ClientBean.class);
+        int count1 = clientBean1.logic();
+        int count2 = clientBean2.logic();
+
+        assertThat(count1).isEqualTo(1);
+        assertThat(count2).isEqualTo(2);
+    }
+
+    @Scope("singleton")
+    @RequiredArgsConstructor
+    static class ClientBean {
+        private final PrototypeBean prototypeBean; // 생성 시점에 주입
+
+        public int logic() {
+            prototypeBean.addCount();
+            return prototypeBean.getCount();
+        }
     }
 
     @Scope("prototype")
