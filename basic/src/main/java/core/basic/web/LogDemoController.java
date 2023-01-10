b106853c26f14b84ffb74cba57f07749fb298f8a
anneu <b.imnuj@gmail.com>
2023-01-10 16:04:44 +0900

    fix: request 스코프 빈 사용 시 Proxy로 문제 해결
    ProxyMode 설정을 통해 초기 빈 등록 시점에 프록시 클래스를 생성해두고, 고객 요청과 상관 없이 프록시 클래스를 다른 빈에 미리 주입해두어 진짜 클래스의 생성을 지연시켜줄 수 있다.
    
    가짜 프록시 객체는 실제 요청이 오면 그때 내부에서 실제 빈을 요청한다.

--- basic/src/main/java/core/basic/web/LogDemoController.java
+++ basic/src/main/java/core/basic/web/LogDemoController.java
@@ -3,7 +3,6 @@ package core.basic.web;
 import core.basic.common.MyLogger;
 import javax.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,14 +11,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequiredArgsConstructor
 public class LogDemoController {
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
 
     @ResponseBody
     @RequestMapping("log-demo")
     public String logDemo(HttpServletRequest request) {
         String requestURL = request.getRequestURI();
 
-        MyLogger myLogger = myLoggerProvider.getObject();
+        System.out.println("myLogger = " + myLogger.getClass());
         myLogger.setRequestURL(requestURL);
         myLogger.log("controller test");
 
