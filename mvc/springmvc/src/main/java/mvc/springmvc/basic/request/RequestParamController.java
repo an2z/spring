fcfe54e4c847d46685d357a6e138093ea44515f9
anneu <b.imnuj@gmail.com>
2023-01-19 15:11:00 +0900

    feat: HTTP 요청 파라미터 조회 - HttpServletRequest 사용 (v1)
    Get 쿼리 파라미터 및 Post HTML Form 방식은 데이터 전송 형식이 같기 때문에 구분 없이 조회가 가능하다. → "요청 파라미터 조회"
    
    HttpServletRequest 객체의 getParameter()를 사용해 요청 파라미터 조회

---
+++ mvc/springmvc/src/main/java/mvc/springmvc/basic/request/RequestParamController.java
@@ -0,0 +1,28 @@
+package mvc.springmvc.basic.request;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+public class RequestParamController {
+	/**
+	 * HttpServletRequest 사용
+	 * 반환 타입이 없으면서 응답에 값을 직접 집어넣으면, view 조회 X
+	 */
+	@RequestMapping("/request-param-v1")
+	public void requestParamV1(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		String username = request.getParameter("username");
+		int age = Integer.parseInt(request.getParameter("age"));
+		log.info("username = {}, age = {}", username, age);
+
+		response.getWriter().write("ok");
+	}
+}
