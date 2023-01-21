907ee000aff30d4aa530143ac6db10791b684029
anneu <b.imnuj@gmail.com>
2023-01-21 14:20:22 +0900

    feat: 프로젝트 생성
    spring mvc 상품 관리 예제

---
+++ mvc/itemservice/src/test/java/mvc/itemservice/ItemServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package mvc.itemservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
