1809c173a36e4281e2a81b66df3056b9f131f518
anneu <b.imnuj@gmail.com>
2023-01-22 20:13:42 +0900

    feat: 상품 상세 컨트롤러 및 뷰 템플릿 개발

--- mvc/itemservice/src/main/java/mvc/itemservice/web/basic/BasicItemController.java
+++ mvc/itemservice/src/main/java/mvc/itemservice/web/basic/BasicItemController.java
@@ -8,6 +8,7 @@ import mvc.itemservice.domain.ItemRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -27,5 +28,12 @@ public class BasicItemController {
         List<Item> items = itemRepository.findAll();
         model.addAttribute("items", items);
         return "basic/items";
+    }
+
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable long itemId, Model model) {
+        Item item = itemRepository.findOne(itemId);
+        model.addAttribute("item", item);
+        return "basic/item";
     }
 }
