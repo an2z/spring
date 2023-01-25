dbe571acc6d6727ed457cd313e009504b38e71a6
anneu <b.imnuj@gmail.com>
2023-01-25 17:56:35 +0900

    refactor: 스프링 컨테이너로 통합

--- toby/src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ toby/src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -9,17 +9,22 @@ import org.springframework.web.servlet.DispatcherServlet;
 public class HellobootApplication {
 
 	public static void main(String[] args) {
-		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext(); // 스프링 컨테이너
-		applicationContext.registerBean(HelloController.class); // 빈 등록
+		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+			@Override
+			protected void onRefresh() {
+				super.onRefresh();
+
+				ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+				WebServer webServer = serverFactory.getWebServer(servletContext -> {
+					servletContext.addServlet("dispatcherServlet", new DispatcherServlet(this))
+						.addMapping("/*");
+				});
+				webServer.start();
+			}
+		};
+		applicationContext.registerBean(HelloController.class);
 		applicationContext.registerBean(SimpleHelloService.class);
 		applicationContext.refresh();
-
-		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-		WebServer webServer = serverFactory.getWebServer(servletContext -> {
-			servletContext.addServlet("dispatcherServlet", new DispatcherServlet(applicationContext))
-				.addMapping("/*");
-		});
-		webServer.start();
 	}
 
 }
