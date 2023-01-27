967e624534170d3ae9df703392ab681a4a6e98d8
anneu <b.imnuj@gmail.com>
2023-01-27 15:59:39 +0900

    feat: 자동 구성 정보 파일 분리

--- toby/src/main/java/tobyspring/config/MyAutoConfigImportSelector.java
+++ toby/src/main/java/tobyspring/config/MyAutoConfigImportSelector.java
@@ -1,14 +1,26 @@
 package tobyspring.config;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.boot.context.annotation.ImportCandidates;
 import org.springframework.context.annotation.DeferredImportSelector;
 import org.springframework.core.type.AnnotationMetadata;
 
 public class MyAutoConfigImportSelector implements DeferredImportSelector {
+	private final ClassLoader classLoader;
+
+	public MyAutoConfigImportSelector(ClassLoader classLoader) {
+		this.classLoader = classLoader;
+	}
+
 	@Override
 	public String[] selectImports(AnnotationMetadata importingClassMetadata) {
-		return new String[] {
-			"tobyspring.config.autoconfig.TomcatServerConfig",
-			"tobyspring.config.autoconfig.DispatcherServletConfig"
-		};
+		List<String> autoConfigs = new ArrayList<>();
+
+		ImportCandidates.load(MyAutoConfiguration.class, classLoader) // 자동 구성에 사용할 Configuration 클래스의 목록을 반환
+			.forEach(autoConfigs::add);
+
+		return autoConfigs.toArray(String[]::new);
 	}
 }
