9dbc70d324b2bdad64c11061a0be0af601efbafa
anneu <b.imnuj@gmail.com>
2023-02-03 21:36:14 +0900

    스트림 무한 생성 (iterate)
    iterate 메서드는 초깃값과 람다를 인수로 받아 새로운 값을 끊임없이 생산할 수 있다.
    → "무한 스트림" = "언바운드 스트림"

---
+++ src/grammar/StreamIterate.java
@@ -0,0 +1,39 @@
+package src.grammar;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class StreamIterate {
+    public static void main(String[] args) {
+        makeFibonacci();
+        makeEven();
+    }
+
+    private static void makeFibonacci() {
+        makeFibonacciSet().stream()
+                .map(t -> t[0])
+                .forEach(System.out::println);
+    }
+
+    private static List<int[]> makeFibonacciSet() {
+        List<int[]> fibonacciSet = Stream.iterate(new int[]{0, 1}, t -> new int[]{t[1], t[0] + t[1]})
+                .limit(20)
+                .collect(Collectors.toList());
+
+        fibonacciSet.forEach(t -> System.out.println("(" + t[0] + "," + t[1] + ")"));
+
+        return fibonacciSet;
+    }
+
+    private static void makeEven() {
+        IntStream.iterate(0, n -> n + 2)
+                .takeWhile(n -> n < 100)
+                .forEach(System.out::println);
+
+        // 두번째 인수: 프레디케이트를 받아 언제까지 작업을 수행할 것인지의 기준으로 사용
+        IntStream.iterate(0, n -> n < 100, n -> n + 2)
+                .forEach(System.out::println);
+    }
+}
