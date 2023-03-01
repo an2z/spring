4c1b858a609ffc0cd18ec58ae95e2cb2b2d2c06f
anneu <b.imnuj@gmail.com>
2023-03-01 09:11:26 +0900

    DataSource 테스트 - 커넥션 풀
    커넥션 풀에서 커넥션을 생성하는 작업은 애플리케이션 실행 속도에 영향을 주지 않기 위해 별도의 쓰레드에서 작동

--- db/jdbc/src/test/java/hello/jdbc/connection/ConnectionTest.java
+++ db/jdbc/src/test/java/hello/jdbc/connection/ConnectionTest.java
@@ -4,6 +4,7 @@ import static hello.jdbc.connection.ConnectionConst.PASSWORD;
 import static hello.jdbc.connection.ConnectionConst.URL;
 import static hello.jdbc.connection.ConnectionConst.USERNAME;
 
+import com.zaxxer.hikari.HikariDataSource;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -27,6 +28,21 @@ class ConnectionTest {
         // DriverManagerDataSource - 항상 새로운 커넥션 획득
         DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD); // DataSource 설정
         useDataSource(dataSource);
+    }
+
+    @Test
+    void dataSourceConnectionPool() throws SQLException, InterruptedException {
+        // 커넥션 풀링
+        HikariDataSource dataSource = new HikariDataSource();
+
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+        dataSource.setMaximumPoolSize(10);
+        dataSource.setPoolName("MyPool");
+
+        useDataSource(dataSource);
+        Thread.sleep(1000); // 쓰레드 풀에 커넥션이 생성되는 로그 확인을 위함
     }
 
     private void useDataSource(DataSource dataSource) throws SQLException {
