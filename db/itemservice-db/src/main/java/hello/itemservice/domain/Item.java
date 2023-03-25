5b06696b6e5d76944ade878e1e434789ef5290d1
anneu <b.imnuj@gmail.com>
2023-03-25 21:39:11 +0900

    JPA 엔티티 - 객체와 테이블 매핑
    • JPA가 관리하는 엔티티는 public 또는 protected의 기본 생성자가 필수이다.
    • JPA는 객체와 테이블을 매핑을 위한 애노테이션을 제공한다. (@Entity, @Id, @Colunm 등)

--- db/itemservice-db/src/main/java/hello/itemservice/domain/Item.java
+++ db/itemservice-db/src/main/java/hello/itemservice/domain/Item.java
@@ -1,12 +1,21 @@
 package hello.itemservice.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import lombok.Data;
 
 @Data
+@Entity
 public class Item {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //PK 값을 DB에서 넣어주는 전략
     private Long id;
 
+    @Column(name = "item_name", length = 10) //name 생략 가능 (카멜케이스->언더스코어 자동 변환)
     private String itemName;
     private Integer price;
     private Integer quantity;
