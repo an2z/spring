5164ad021ed2f4c1411b31646d9073935c613b29
anneu <b.imnuj@gmail.com>
2023-04-14 18:33:51 +0900

    트랜잭션 AOP 주의사항 - 프록시 내부 호출 문제 해결
    • 트랜잭션을 사용하는 메서드를 별도의 클래스로 분리해 호출하면 트랜잭션이 적용됨

--- db/springtx/src/test/java/hello/springtx/apply/InternalCallTest.java
+++ db/springtx/src/test/java/hello/springtx/apply/InternalCallTest.java
@@ -1,5 +1,6 @@
 package hello.springtx.apply;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,11 +22,6 @@ public class InternalCallTest {
     }
 
     @Test
-    void internalCall() {
-        callService.internal();
-    }
-
-    @Test
     void externalCall() {
         callService.external();
     }
@@ -34,22 +30,36 @@ public class InternalCallTest {
     static class InternalCallTestConfig {
         @Bean
         CallService callService() {
-            return new CallService();
+            return new CallService(internalService());
+        }
+
+        @Bean
+        InternalService internalService() {
+            return new InternalService();
         }
     }
 
     @Slf4j
+    @RequiredArgsConstructor
     static class CallService {
+        private final InternalService internalService;
+
         /**
-         * external()은 트랜잭션이 없지만 내부에 트랜잭션이 있는 internal()를 호출하기 때문에 트랜잭션이 적용되는 것처럼 보임
-         * but, 실제로 internal()에서 트랜잭션이 전혀 적용되지 않음
+         * internal()을 별도의 클래스로 분리하여 호출하면 트랜잭션이 적용됨
          */
         public void external() {
             log.info("call external");
             printTxInfo();
-            internal();
+            internalService.internal();
         }
 
+        private void printTxInfo() {
+            boolean txActive = TransactionSynchronizationManager.isActualTransactionActive();
+            log.info("tx active = {}", txActive);
+        }
+    }
+
+    static class InternalService {
         @Transactional
         public void internal() {
             log.info("call internal");
